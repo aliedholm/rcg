84c0838e17fb159f9c7cad05da645186c04cb4ef
Andrew Liedholm <aliedholm@gmail.com>
2017-08-01 16:00:52 -0700

    added some error handling working on pushing new data to subdocument arrays

--- node/app_api/controllers/tanks.js
+++ node/app_api/controllers/tanks.js
@@ -25,3 +25,117 @@ module.exports.biofilterTanks = function(req, res) {
 module.exports.reservoirTanks = function(req, res) { 
   sendJsonResponse(res, 200, {"status" : "success"})
 };
+
+module.exports.tankByName = function(req, res) {
+  if (req.params.tankName) {
+		tankModel
+			.find({"tankName" : req.params.tankName})
+			.exec(function(err, tankData) {
+        if (!tankData.length) {
+          sendJsonResponse(res, 404, { "message" : "tank not found" });
+          console.log("tank not found error");
+          return;
+        }
+        else if (err) { 
+          sendJsonResponse(res, 404, err);
+          console.log("error object passed down");
+          return;
+        }
+				sendJsonResponse(res, 200, {"status" : tankData});
+			})
+  }
+  else {
+    sendJsonResponse(res, 404, { "message" : "No tank name in request" });
+    console.log("No tank name in request");
+  }
+}
+
+module.exports.tankStats = function(req, res) {
+  if(req.params.tankName && req.params.stat) {
+		tankModel
+			.find({"tankName" : req.params.tankName})
+			.select(req.params.stat)
+			.exec(function(err, data) {
+        if(!data) {
+          sendJsonResponse(res, 404, {"message" : "data not found"});
+          return;
+        }
+        else if(err) {
+          sendJsonResponse(res, 404, err);
+          return;
+        }
+				sendJsonResponse(res, 200, {"status" : data})
+			})
+  }
+  else {
+    sendJsonResponse(res, 404, {"message" : "incomplete request"});
+  }
+}
+
+module.exports.tankCreate = function(req, res) {
+  tankModel.create({
+    tankName: req.body.tankName,
+    tankType: req.body.tankType,
+    tankNumber: req.body.tankNumber,
+    phData: [],
+    doData: [],
+    ecData: [],
+    wtempData: [],
+    wlvlData: [],
+  }, function(err, tank) {
+    if(err) {
+      sendJsonResponse(res, 400, err);
+    }
+    else {
+      sendJsonResponse(res, 201, tank);
+    }
+  })
+}
+
+module.exports.statAdd = function(req, res) {
+  var tankName = req.params.tankName;
+  var statName = req.params.statName;
+  if (tankName && statName) {
+		tankModel
+			.find({"tankName" : req.params.tankName})
+      .select(statName)
+			.exec(function(err, statData) {
+        if (!statData.length) {
+          sendJsonResponse(res, 404, { "message" : "tank not found" });
+          console.log("tank not found error");
+          return;
+        }
+        else if (err) { 
+          sendJsonResponse(res, 404, err);
+          console.log("error object passed down");
+          return;
+        }
+        doAddData(req, res, statData);
+			})
+  }
+  else {
+    sendJsonResponse(res, 404, { "message" : "No tank name and/or stat in request" });
+    console.log("No tank name and/or stat in request");
+  }
+}
+
+var doAddData = function(req, res, tankData) {
+  if(!tankData) {
+    sendJsonResponse(res, 404, {"message" : "tank not found"});
+  } else {
+    tankData.push({
+      timestamp: new Date(),
+      reading: req.body.reading
+    });
+  }
+  sendJsonResponse(res, 201, tankData);
+  tankData.save(function(err, tankData) {
+    var thisData;
+    if(err) {
+      sendJsonResponse(res, 400, err);
+    } else {
+      thisData = tankData.statName[tankData.statName.length - 1];
+      sendJsonResponse(res, 201, thisData);
+    }
+  });
+}   
--- node/app_api/routes/index.js
+++ node/app_api/routes/index.js
@@ -4,9 +4,20 @@ var router = express.Router();
 var ctrlTanks = require('../controllers/tanks');
 
 //tanks api definitions
-router.get('/tanks', ctrlTanks.allTanks);
+router.get('/tanks/', ctrlTanks.allTanks);
 router.get('/tanks/fish', ctrlTanks.fishTanks);
 router.get('/tanks/biofilter', ctrlTanks.biofilterTanks);
 router.get('/tanks/reservoir', ctrlTanks.reservoirTanks);
+router.get('/tanks/name/:tankName', ctrlTanks.tankByName);
+router.get('/tanks/name', ctrlTanks.tankByName);
+
+router.get('/stats', ctrlTanks.tankStats);
+router.get('/stats/:tankName', ctrlTanks.tankStats);
+router.get('/stats/:tankName/:stat', ctrlTanks.tankStats);
+
+router.post('/tanks', ctrlTanks.tankCreate);
+router.post('/tanks/:tankName', ctrlTanks.statAdd);
+router.post('/tanks/:tankName/stats', ctrlTanks.statAdd);
+router.post('/tanks/:tankName/stats/:statName', ctrlTanks.statAdd);
 
 module.exports = router;
