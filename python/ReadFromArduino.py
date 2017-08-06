fe1c583f5e98fc087e799991fb0f69b96cc97885
YimengYang <yiy170@ucsd.edu>
2017-08-06 23:40:34 +0000

    successfully post on the website

--- python/ReadFromArduino.py
+++ python/ReadFromArduino.py
@@ -1,29 +1,31 @@
 import serial
-import json
-""" Keep reading from one Arduino, parse the read and send data to database
-Args:
-	db(mongodb database): The database we use for all data
-	port(str): The port where Arduino is connected to the pi
-"""
 import sys
 sys.path.append("~/Github/rcg/python/")
+from ParseAsUrl import ParseAsUrl
+import requests
 
-from ParseAsJson import ParseAsJson
-from datetime import datetime
-
-def ReadFromArduino(db,port):
+def ReadFromArduino(port):
+	""" Keep reading from one Arduino, parse the read and send data to 
+	database
+	
+	Parameter:
+	port(str): The port where Arduino is connected to the pi
+	"""
 
 	ser = serial.Serial(port,9600)
 	
 	while True:
-		# Keep reading
-		if(ser.inWaiting()>0):
-			reading = ser.readline()
-			timestamp = str(datetime.now())	
-			
-			result_dict,tank_type=ParseAsJson(timestamp,reading)
-			db[tank_type].insert_one(result_dict)
-
+		#Handle exception by throwing bad data and keep looping
+		try:	
+			# Keep reading
+			if(ser.inWaiting()>0):
+				reading = ser.readline()
+				url, value=ParseAsUrl(reading)
+				
+				# Post the value to the url
+				requests.post(url, data = {'reading':value})
+		except (ValueError,IndexError):
+			continue
 			
 	
 		
