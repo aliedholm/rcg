dc96d961bf1c5cf74138185f1dd583695c226b6e
Andy <aliedholm@gmail.com>
2018-08-14 18:44:07 -0700

    fixed arduino master sketch to accept commands as 0-9, but cannot seem to figure out how to send larger commands yet. Outside of that temperature and relay control work. remaned python sketch to relfect function better.

--- arduino/master-arduino-sketch/master-arduino-sketch.ino
+++ arduino/master-arduino-sketch/master-arduino-sketch.ino
@@ -2,15 +2,30 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #define ONE_WIRE_BUS_PIN 2
+
 OneWire oneWire(ONE_WIRE_BUS_PIN);
 DallasTemperature sensors(&oneWire);
+
+  //device addresses to be found in another sketch and assigned here
 DeviceAddress Probe01 = { 0x28, 0xFF, 0xE0, 0x7F, 0xB5, 0x16, 0x05, 0x50 }; 
 DeviceAddress Probe02 = { 0x28, 0xFF, 0x50, 0xEF, 0xC1, 0x16, 0x04, 0xD1 };
 DeviceAddress Probe03 = { 0x28, 0xFF, 0x7B, 0xCC, 0xC1, 0x16, 0x04, 0xA1 };
 DeviceAddress Probe04 = { 0x28, 0xFF, 0x4F, 0x19, 0xC2, 0x16, 0x04, 0x5E };
 //ds18b20 code end
 
+//for relay control code
+// declare and name all actuator pins on the arduino
+const int r1 = 13;
+const int r2 = 9;
+const int r3 = 10;
+const int r4 = 11;
+//relay control code end
+
+//declare command code variable
+int commandCode = 0;
+
 void setup() {
+  
   Serial.begin(9600);
   
   //for ds18b20 code
@@ -20,45 +35,96 @@ void setup() {
   sensors.setResolution(Probe03, 12);
   sensors.setResolution(Probe04, 12);
   //ds18b20 code end
+
+  //relay control code
+  pinMode(r1, OUTPUT);
+  pinMode(r2, OUTPUT);
+  pinMode(r3, OUTPUT);
+  pinMode(r4, OUTPUT);
+  //relay control code end
   
 }
 
 void loop() {
-  delay(150);
   
-  int commandCode = 0;
+  //ds18b20 generic code
+  sensors.requestTemperatures();
+  delay(100);
+  //end ds18b20 generic code
+
+  // read serial data if its available and set command code
   if(Serial.available()){
     commandCode = Serial.read() - '0';
-  }  
+    delay(50);
+  }   
   
-  switch (commandCode) {
-    
-    //request one round of temperature data
-    case 119:
-      sensors.requestTemperatures();
-      printTemperature(Probe01);
-      break;
+  //Main set of if statements to trigger conditions from Serial commands
       
-    case 129:
-      sensors.requestTemperatures();
-      printTemperature(Probe02);
-      break;
-
-    case 139:
-      sensors.requestTemperatures();
-      printTemperature(Probe03);
-      break;
-
-    case 149:
-      sensors.requestTemperatures();
-      printTemperature(Probe04);
-      break;
-      
-    default:
-      delay(100);
+  if(commandCode == 1){
+    Serial.print("Temperature Probe 1: ");
+    printTemperature(Probe01);
+    commandCode = 0;
+  }
+  
+  if(commandCode == 2){
+    Serial.print("Temperature Probe 2: ");
+    printTemperature(Probe02);
+    commandCode = 0;
+  }
+  
+  if(commandCode == 3){
+    Serial.print("Temperature Probe 3: ");
+    printTemperature(Probe03);
+    commandCode = 0;
+  }
+  
+  if(commandCode == 4){
+    Serial.print("Temperature Probe 4: ");
+    printTemperature(Probe04);
+    commandCode = 0;
   }
 
-}
+  if(commandCode == 5){
+    Serial.println("Relay 1 on");
+    digitalWrite(r1, HIGH);
+    commandCode = 0;
+  }
+
+  if(commandCode == 6){
+    Serial.println("Relay 1 off");
+    digitalWrite(r1, LOW);
+    commandCode = 0;
+  }
+  
+  if(commandCode == 7){
+    Serial.println("Relay 2 on");
+    digitalWrite(r2, HIGH);
+    commandCode = 0;
+  }
+  
+  if(commandCode == 8){
+    Serial.println("Relay 2 off");
+    digitalWrite(r2, LOW);
+    commandCode = 0;
+  }
+  
+  if(commandCode == 9){
+    Serial.println("Relay 3 on");
+    digitalWrite(r3, HIGH);
+    commandCode = 0;
+  }
+  
+  if(commandCode == 10){
+    Serial.println("Relay 3 off");
+    digitalWrite(r3, LOW);
+    commandCode = 0;
+  }
+  
+//End of Main set of if statements 
+        
+}//end of main loop
+
+//start of functions needed to be called in them main loop
 
 //ds18b20 code functions
 void printTemperature(DeviceAddress deviceAddress)
--- python/python-send.py
+++ python/python-send.py
@@ -3,9 +3,7 @@ import sys
 
 options = sys.argv[1]
 arduinoNum = options[0:1]
-device = options[1:3]
-toggle = options[3:4]
-command = options [1:4]
+command = options [1:3]
 
 ardSerial = serial.Serial('/dev/ttyACM' + arduinoNum, 9600)
 
