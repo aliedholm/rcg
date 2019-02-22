9f4c84c250189712e46fb92696a3bf4472b410df
Shera <xil475@ucsd.edu>
2019-02-21 17:41:49 -0800

    greeting

--- python/serial-test-code/my-pyserial-code.py
+++ python/serial-test-code/my-pyserial-code.py
@@ -1,12 +1,22 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/cu.usbmodem1411', 9600)
+ser1 = serial.Serial('/dev/cu.usbmodem14201', 9600)
+ser2 = serial.Serial('/dev/cu.usbmodem14101', 9600)
+
 while True:
-    user_input = input('happy: ').encode('latin-1') + b'\n'
-    ser.write(user_input)
-    while ser.in_waiting <= 0:
+    # user_input = input('happy: ').encode('latin-1') + b'\n'
+    # ser1.write(user_input)
+    ser1.write(b"I am fine,how are you ? \n" )
+    ser2.write(b"I am good, and you? \n")
+
+    while ser1.in_waiting <= 0:
         time.sleep(0.05)
-    while ser.in_waiting > 0:
-        print(ser.readline())
+    while ser1.in_waiting > 0:
+        print("ser1 saying: " + ser1.readline().decode("utf-8"))
+
+    while ser2.in_waiting <= 0:
+        time.sleep(0.05)
+    while ser2.in_waiting > 0:
+        print("ser2 saying: " + ser2.readline().decode("utf-8"))    
     
