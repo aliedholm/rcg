f33ada2e686537d428af0b8a7e3431e627094a1d
EpicOrange@github.com <danieldoesntcheckthis@gmail.com>
2019-02-21 16:55:47 -0800

    Update serial test code to work

--- python/serial-test-code/my-pyserial-code.py
+++ python/serial-test-code/my-pyserial-code.py
@@ -1,6 +1,12 @@
 import serial
+import time
+
 ser = serial.Serial('/dev/cu.usbmodem1411', 9600)
 while True:
-    ser.write(b'hello\n')
+    user_input = input('happy: ').encode('latin-1') + b'\n'
+    ser.write(user_input)
+    while ser.in_waiting <= 0:
+        time.sleep(0.05)
     while ser.in_waiting > 0:
         print(ser.readline())
+    
