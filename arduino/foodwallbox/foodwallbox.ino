3dcf767c0be91f6acfa462abbdcd1132eb59db1c
Andy <aliedholm@gmail.com>
2019-05-30 16:29:22 -0700

    added foodwallbox arduino sketch (working)

---
+++ arduino/foodwallbox/foodwallbox.ino
@@ -0,0 +1,43 @@
+int pin = 6; // Analog pin in that 
+
+#define floatPin 4  // Digital pin that float value is connected to
+#define switchPin 2 // Digital pin that switch is connected to
+#define relayPin 6 // Digital pin that switch is connected to
+
+int sensorValue;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(100);
+
+  pinMode(4, INPUT);
+  pinMode(2, INPUT);
+  pinMode(6, OUTPUT);
+}
+
+void loop() {
+   
+  int floatValue = digitalRead(floatPin);
+  Serial.print("Float Value");
+  Serial.println(floatValue);
+
+  int switchValue = digitalRead(switchPin);
+  Serial.print("Switch Value");
+  Serial.println(switchValue);
+  
+  if(switchValue == HIGH && floatValue == HIGH) {
+    digitalWrite(relayPin, LOW);
+  }
+  if(switchValue == LOW && floatValue == LOW) {
+    digitalWrite(relayPin, HIGH);
+  }
+  if(switchValue == LOW && floatValue == HIGH) {
+    digitalWrite(relayPin, LOW);
+  }
+  if(switchValue == HIGH && floatValue == LOW) {
+    digitalWrite(relayPin, LOW);
+  }    
+  delay(500); 
+
+}
