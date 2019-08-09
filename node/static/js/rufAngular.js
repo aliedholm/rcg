12f56102c1bffb0a4a4723ba5d665414f2d4f9f8
Andrew Liedholm <aliedholm@gmail.com>
2019-08-09 01:46:05 +0000

    got site up and running and switching sensors smoothly, next is switching databases and then paginating days

--- node/static/js/rufAngular.js
+++ node/static/js/rufAngular.js
@@ -4,13 +4,24 @@ angular.module('rufAngular', [])
 //dataCtrl controller definition
   .controller('dataCtrl', function(httpService){
     var self = this;
-    self.title = 'This is the title ';
-    self.fakeTitle = 'This is not the title ';
+    self.databases = ["sensors", "logs", "digester"]; 
+    self.currentSensor = "table61";
+    self.currentDatabase = "sensors";
 
 //URLs of respective data or buildling the URL strings
     var sensorsUrl = 'http://132.239.205.188:8080/api/retrieveTables/sensors';
-    var table61Url = 'http://132.239.205.188:8080/api/retrieveTable/sensors-table61';
-    var temp1Url = 'http://132.239.205.188:8080/api/retrieveTable/sensors-temp1';
+
+//function to build database URLs
+    var builDatabaseUrl = function(database){
+      url = "http://132.239.205.188:8080/api/retrieveTables/" + database;
+      return url;
+    }
+
+//function to build sensor URLs
+    var buildSensorUrl = function(database, sensor){
+      url = "http://132.239.205.188:8080/api/retrieveTable/" + database + "-" + sensor;
+      return url;
+    }
 
 //Call for all the sensor names
     httpService.getData(sensorsUrl)
@@ -20,17 +31,21 @@ angular.module('rufAngular', [])
 
 //Call for the initial data
       .then(function(results){
-        httpService.getData(temp1Url)
-          .then(function(response){
-            self.dataPoints = response;
-          });    
+        self.fetchNew("table61");
       });
-      
-      self.fetchNew = function(url){
-        httpService.getData(temp1Url)
+   
+//function to fetch new data and update the page 
+      self.fetchNew = function(sensor){
+        url = buildSensorUrl(self.currentDatabase, sensor);
+        httpService.getData(url)
           .then(function(response){
+            self.currentSensor = sensor;
             self.dataPoints = response;
           });    
+      }
+      
+      self.changeDb = function(database){
+        self.currentDatabase = database;
       }
   })
 
