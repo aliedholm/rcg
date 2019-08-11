44081f586c9626fc951e2bd2f3b587d45fbaa444
Andrew Liedholm <aliedholm@gmail.com>
2019-08-11 16:23:54 +0000

    got page working right on first load

--- node/static/js/rufAngular.js
+++ node/static/js/rufAngular.js
@@ -6,71 +6,77 @@ angular.module('rufAngular', [])
   //variable definitions for init
     var self = this;
     self.databases = ["sensors", "logs", "digester"]; 
-    self.currentSensor = "table31";
     self.currentDatabase = self.databases[0];
-    self.startDate = getCurrentDate(-1);
-    self.endDate = getCurrentDate;
-
-//initial page setup calls on load
-  //Call for all the sensor names
-    httpService.getData(buildDatabaseUrl(self.currentDatabase))
-      .then(function(response){
-        self.sensors = response;
-      })
-  //Call for the initial dates
-      .then(function(results){
-        self.fetchNewSensor(self.currentSensor);
-      })
-  //Call for the initial data
-      .then(function(results){
-        self.fetchNewDates(self.dateOldest, self.dateNewest);
-      })
+    self.startDate = "2000-01-01";
+    self.endDate = "3000-01-01";
+    self.dateOldest = "2000-01-01";
+    self.dateNewest = "3000-01-01";
+    
+//function to setup page fresh for new DB change
+    self.changeDb = function(database){
+      self.currentDatabase = database;
+      httpService.getData(buildDatabaseUrl(self.currentDatabase))
+        .then(function(response){
+          self.sensors = response;
+          self.currentSensor = response[0];
+        })
+        .then(function(results){
+          self.fetchDistinctDates(self.currentSensor);
+        })
+        .then(function(results){
+          self.fetchNewSensorDates(self.startDate, self.endDate);
+        })
+    } 
 
 //data fetching functions to assist in grabbing data 
   //function to fetch new list of tables from DB and update the page
-      self.fetchTables = function(database){
-        url = buildDatabaseUrl(database);
-        httpService.getData(url)
-          .then(function(response){
-            self.sensors = response;
-          });
-      }
+    self.fetchTables = function(database){
+      url = buildDatabaseUrl(database);
+      httpService.getData(url)
+        .then(function(response){
+          self.sensors = response;
+        });
+    }
 
   //function to fetch new data from a sensor table and update the page 
-      self.fetchNewSensor = function(sensor){
-        self.currentSensor = sensor;
-        url = buildSensorUrl(self.currentDatabase, self.currentSensor);
-        httpService.getData(url)
-          .then(function(response){
-            self.dataPoints = response;
-          })
-          .then(function(dates){
-            self.fetchDistinctDates(self.currentSensor);
-          });
-      }
+    self.fetchNewSensor = function(sensor){
+      self.currentSensor = sensor;
+      url = buildSensorUrl(self.currentDatabase, self.currentSensor);
+      httpService.getData(url)
+        .then(function(response){
+          self.dataPoints = response;
+        })
+        .then(function(dates){
+          self.fetchDistinctDates(self.currentSensor);
+        });
+    }
 
   //function to fetch new data from a sensor table by date and update the page 
-      self.fetchNewDates = function(start, end){
-        self.dataStart = start;
-        self.dataEnd = end;
-        var url = buildSensorDatesUrl(self.currentDatabase, self.currentSensor);
-        var params = { "start" : start, "end" : end };
-        httpService.getData(url, params)
-          .then(function(response){
-            self.dataPoints = response;
-          })    
-      }
+    self.fetchNewSensorDates = function(start, end){
+      self.dateOldest = start;
+      self.dateNewest = end;
+      var url = buildSensorDatesUrl(self.currentDatabase, self.currentSensor);
+      var params = { "start" : start, "end" : end };
+      httpService.getData(url, params)
+        .then(function(response){
+          self.dataPoints = response;
+        })    
+    }
 
   //function to fetch the list of dates avaialable by sensor
-      self.fetchDistinctDates = function(sensor){
-        url = buildDatesUrl(self.currentDatabase, sensor);
-        httpService.getData(url)
-          .then(function(response){
-            self.datesAvailable = response;
-            self.dateNewest = response[0];
-            self.dateOldest = response[response.length - 1];
-          });
-      }
+    self.fetchDistinctDates = function(sensor){
+      url = buildDatesUrl(self.currentDatabase, sensor);
+      httpService.getData(url)
+        .then(function(response){
+          self.datesAvailable = response;
+          self.dateNewest = response[0];
+          self.dateOldest = response[response.length - 1];
+          self.endDate = response[0];
+          self.startDate = response[response.length - 1];
+        });
+    }
+//initialize the page by loading the default db
+    self.changeDb(self.currentDatabase);
 
   })
 //end of controller function
